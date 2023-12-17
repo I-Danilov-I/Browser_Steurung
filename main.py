a7b7520f4294988e693c587b5e9a81698a8e64dc
ARHITEKTOR <danilov@mail.de>
2023-12-17 18:40:13 +0100

    Suchleiste finden, Text Eingabe, Button anklicken (Suche Starten)

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from selenium.webdriver import Chrome, ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 options = ChromeOptions()
 options.add_argument("--start-maximized")
 
 driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get("https://www.youtube.com/")
+driver.get("https://www.wikipedia.de")
 time.sleep(10)
+
+search_bar = driver.find_element(By.ID, 'txtSearch')
+search_bar.send_keys('Salzburg')
+
+button = driver.find_element(By.ID, 'cmdSearch')
+button.click()
+
+article_element = driver.find_element(By.ID, 'content')
+article_text = article_element.text
+print(article_text)
+
+time.sleep(1000)
+driver.quit()
